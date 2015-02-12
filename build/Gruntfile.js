8284d14656e80e0df54119f449861b7f4482ced1
Renato Pereira <renato.ppontes@gmail.com>
2015-02-12 11:06:31 -0200

    Build process updated, plus a dev release

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
       },
       dist: {
         src: [],
-        dest: '../libs/<%= project %>.<%= pkg.version %>.js',
+        dest: '../libs/<%= project %>-<%= pkg.version %>.js',
       }
     },
     uglify: {
@@ -20,24 +20,8 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
       },
       build: {
-        src: '../libs/<%= project %>.<%= pkg.version %>.js',
-        dest: '../libs/<%= project %>.<%= pkg.version %>.min.js'
-      }
-    },
-    cssmin: {
-      dist: {
-        src: [],
-        dest: '../libs/<%= project %>.<%= pkg.version %>.min.css',
-      }
-    },
-    htmlmin: {
-      dist: {
-        options: {
-          removeComments: true,
-          collapseWhitespace: true
-        },
-        src: [],
-        dest: '../libs/<%= project %>.<%= pkg.version %>.min.html',
+        src: '../libs/<%= project %>-<%= pkg.version %>.js',
+        dest: '../libs/<%= project %>-<%= pkg.version %>.min.js'
       }
     },
     yuidoc: {
@@ -62,7 +46,7 @@ module.exports = function(grunt) {
       build: {
         options: {
           mode:'zip',
-          archive:'../docs/<%= project %>.<%= pkg.version %>.zip'
+          archive:'../docs/<%= project %>-<%= pkg.version %>.zip'
         },
         files: [
           {expand:true, src:'**', cwd:'../docs/behavior3js'}
@@ -74,8 +58,6 @@ module.exports = function(grunt) {
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-yuidoc');
   grunt.loadNpmTasks('grunt-contrib-compress');
 
@@ -86,39 +68,12 @@ module.exports = function(grunt) {
 
   // CORE
   grunt.registerTask('_core', function() {
-    grunt.config.set('project', 'b3core');
+    grunt.config.set('project', 'behavior3');
     grunt.config.set('concat.dist.src', files['core_source']);
-  });
-
-  // VIEW
-  grunt.registerTask('_view', function() {
-    grunt.config.set('project', 'b3view');
-    grunt.config.set('concat.dist.src', files['view_source']);
-  });
-
-  // EDITOR
-  grunt.registerTask('_editor', function() {
-    grunt.config.set('project', 'b3editor');
-    grunt.config.set('concat.dist.src', files['editor_source']);
-  });
-  grunt.registerTask('_editor_html', function() {
-    grunt.config.set('project', 'b3editor');
-    grunt.config.set('concat.dist.src', files['editor_html']);
-    grunt.config.set('concat.dist.dest', '../libs/<%= project %>.<%= pkg.version %>.html');
-    grunt.config.set('htmlmin.dist.src', '../libs/<%= project %>.<%= pkg.version %>.html');
-  });
-  grunt.registerTask('_editor_css', function() {
-    grunt.config.set('project', 'b3editor');
-    grunt.config.set('cssmin.dist.src', files['editor_css']);
   });
 
   // Default task(s).
   grunt.registerTask('docs', ['_docs', 'yuidoc', 'compress']);
-  grunt.registerTask('core', ['_core', 'concat', 'uglify']);
-  grunt.registerTask('view', ['_view', 'concat', 'uglify']);
-  grunt.registerTask('editor', ['editor_app', 'editor_html','editor_css']);
-  grunt.registerTask('editor_app', ['_editor', 'concat', 'uglify']);
-  grunt.registerTask('editor_css', ['_editor_css', 'cssmin']);
-  grunt.registerTask('editor_html', ['_editor_html', 'concat', 'htmlmin']);
+  grunt.registerTask('build', ['_core', 'concat', 'uglify']);
 
 };
